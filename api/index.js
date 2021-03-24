0488a7a5f7ec77c7330fde1e8c26545ced92669f
Parn Boonyamanee <parnpresso@gmail.com>
2021-03-24 17:40:44 +0700

    Add replicas :crown:

--- api/index.js
+++ api/index.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  console.log('Request: /')
   res.send('Hello World!')
+})
+
+app.get('/crash', (req, res) => {
+  console.log('Request: /crash')
+  process.exit()
 })
 
 app.listen(port, () => {
